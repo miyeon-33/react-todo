62dfe96085b153ed04c53906edc55d798300e261
miyeon-33 <elav0303@gmail.com>
2025-03-28 16:30:22 +0900

    feat: 투두리스트완료 #1

---
+++ src/components/Checkbox.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Checkbox({
+  label,
+  checked,
+  onChange,
+  name,
+  className,
+}) {
+  return (
+    <label className={`checkbox ${className}`}>
+      <input
+        type="checkbox"
+        className="sr-only"
+        checked={checked}
+        onChange={onChange}
+        name={name}
+      />
+      <span className="line-clamp-2">{label}</span>
+    </label>
+  );
+}
